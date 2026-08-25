c6145d39410b44194ced47b3acf9a91c2ad9e8eb
gursimrran <st25169@ormiston.school.nz>
2026-08-25 12:04:11 +1200

    Imports the json module so the program can work with JSON data.

---
+++ app.py
@@ -0,0 +1,12 @@
+import json 
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
+
+app = Flask(__name__)
+app.secret_key= 'your_secret_key'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
