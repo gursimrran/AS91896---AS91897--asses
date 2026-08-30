d08bf58823853bd6b93a5c6816e58267bf5a4991
gursimrran <st25169@ormiston.school.nz>
2026-08-30 19:35:40 +1200

    Add routes for about, checkout, and orders pages

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ app.secret_key= 'your_secret_key'
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('invoice.html')
+
+@app.route('/orders')
+def order_history():
+    return render_template('order_history.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
